18f7c9c64914600286568d1dd935e7e8061c106e
Ian Kao <iankao.lb@gmail.com>
2026-05-05 16:18:34 +0800

    Add HomeView component with router setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Home</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+}
+</style>
